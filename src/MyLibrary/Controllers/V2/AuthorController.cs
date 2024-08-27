01168a456454c26c506b07bbdd075505c439232f
Samuel Kastberg <samuel.kastberg@live.se>
2024-08-27 22:39:51 +0200

    Updated Controllers and UI

--- src/MyLibrary/Controllers/V2/AuthorController.cs
+++ src/MyLibrary/Controllers/V2/AuthorController.cs
@@ -8,10 +8,10 @@ namespace MyLibrary.Controllers.V2;
 /// <summary>
 /// Controller for managing Authors. Version 2.0.
 /// </summary>
-[ApiVersion(2)]
 [ApiVersion("2.0")]
 [Route("api/[controller]")]
 [ApiController]
+[Produces("application/json")]
 public class AuthorController : Controller
 {
     /// <summary>Returns a list of books by a specific author.</summary>
@@ -21,7 +21,6 @@ public class AuthorController : Controller
     [Route("{author}/books")]
     [ProducesResponseType(200, Type = typeof(List<Book>))]
     [ProducesResponseType(404)]
-    [Produces("application/json")]
     public IActionResult GetBooksByAuthor(string author)
     {
         var books = BooksMock.GetBooks().Where<Book>(b => b.Author == author);
@@ -38,7 +37,6 @@ public class AuthorController : Controller
     [HttpGet]
     [MapToApiVersion("2.0")]
     [ProducesResponseType(200, Type = typeof(List<String>))]
-    [Produces("application/json")]
     public IActionResult GetAuthors()
     {
         
